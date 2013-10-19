482a9e0eb587ed8dce098f74361d3e31af6e50fd
Scott Garner <scott@j38.net>
2013-10-18 21:03:41 -0400

    Initial popcorn work

---
+++ js/script.js
@@ -0,0 +1,56 @@
+function loadScript (pop) {
+
+	pop.code({
+			start: "01:15",
+			onStart: function( options ) {
+				helloEditor.videoMode = false;
+				helloEditor.resizeUI();
+			}
+		})
+		.code({
+			start: "01:22",
+			onStart: function( options ) {
+				$("#editorContainer").show();
+				helloEditor.editor.setValue();
+			},
+			onEnd: function( options ) {
+				$("#editorContainer").hide();
+			}
+		})
+		.code({
+			start: "01:27",
+			onStart: function( options ) {
+				helloEditor.editor.setValue("size(600, 600)\n\nellipse(300, 300, 400, 400);");
+			},
+			onEnd: function( options ) {
+				helloEditor.editor.setValue("");
+			}
+		})
+		.code({
+			start: "02:14",
+			onStart: function( options ) {
+				$("#commands").show();
+			},
+			onEnd: function( options ) {
+				$("#commands").hide();
+			}			
+		})
+		.code({
+			start: "02:41",
+			onStart: function( options ) {
+				$("#canvasContainer").show();
+			},
+			onEnd: function( options ) {
+				$("#canvasContainer").hide();
+			}			
+		})
+		.code({
+			start: "02:45",
+			onStart: function( options ) {
+				helloEditor.runCode();
+			},
+			onEnd: function( options ) {
+				resetInstance();
+			}			
+		});			
+}
