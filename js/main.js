b75c7fa4e69ae0ed0bb225ae36fc409dc58a68a4
Scott Garner <scott@j38.net>
2013-11-09 16:23:10 -0500

    Splash styling

--- js/main.js
+++ js/main.js
@@ -22,20 +22,23 @@ var helloSplash = {
 	    		"margin-top": $("#splash").height() /-2,
 	    		"margin-left": $("#splash").width() /-2	    		
 			});
-	    
+
+	    $("#splashButton").css(
+	    	{
+	    		"top": "50%",
+	    		"left": "50%",
+	    		"margin-top": ($("#splashButton").height() /-2) + 128,
+	    		"margin-left": $("#splashButton").width() /-2	    		
+			});
+
 	    // Animate In
 
-	    $("#splash").css({'opacity': 0}).animate(
-	      { opacity: 1},
-	      { duration: 600}
-	    );
+	    $("#splash").delay(1400).fadeIn(1200);
+	    $("#splashButton").delay(2600).fadeIn("slow");
 
 	    // Show Footer
 
-	    $("#splashFooter").css({'opacity': 0}).delay(600).animate(
-	      { opacity: 1},
-	      { duration: 'slow'}
-	    );    
+	    $("#splashFooter").delay(3000).fadeIn("slow");
 	}
 }
 
