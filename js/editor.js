01c7d9b464ecacab4a18fb6bb9567d79c215c959
Scott Garner <scott@j38.net>
2013-11-09 23:13:25 -0500

    CSS tweaks for elements over canvas.

--- js/editor.js
+++ js/editor.js
@@ -361,7 +361,7 @@ var helloEditor = {
 			if (typeof this.processingInstance.draw === 'function')
 				this.processingInstance.draw();
 
-			helloEditor.runCache[helloEditor.lessonIndex] = pthis.editor.getValue();
+			helloEditor.runCache[helloEditor.lessonIndex] = this.editor.getValue();
 		}
 		catch(e) {
 			helloEditor.displayError(e);		
