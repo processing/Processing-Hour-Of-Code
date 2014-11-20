88d60e92843ce40a369461e82ebab1f27392f993
Scott Garner <scott@j38.net>
2014-11-20 10:29:18 -0800

    Display page updates for gallery per #64

--- js/display.js
+++ js/display.js
@@ -21,8 +21,13 @@ var helloDisplay = {
         // Get the gistID from the URL and display it
 
         if (document.URL.indexOf('#') >= 0) {
-            var gistID = document.URL.split('#')[1];
-            this.displayGist(gistID);
+            var sketchID = document.URL.split('#')[1];
+
+            if (sketchID[0] == "@") {
+                this.displayGallery(sketchID.substr(1));
+            } else {
+                this.displayGist(sketchID);
+            }
         }
 
         $("#displayCode").click(function () {
@@ -66,37 +71,66 @@ var helloDisplay = {
 
                 var gistFiles = data.responseJSON.files,
                     gistFile = gistFiles[Object.keys(gistFiles)[0]],
-                    gistSource = gistFile.content,
-                    processingCanvas,
-                    processingInstance,
-                    displayHeight,
-                    displayWidth;
+                    gistSource = gistFile.content;                    
 
-                helloDisplay.processingSource = gistSource;
-                helloDisplay.editor.setValue(gistSource, -1);
-
-                try {
-                    processingCanvas = document.getElementById("displayCanvas");
-                    processingInstance = new Processing(processingCanvas, gistSource);
-
-                    displayHeight = 400;
-                    displayWidth = (processingInstance.width / processingInstance.height) * displayHeight;
-
-                    $("#displayCanvas").css({
-                        width: displayWidth,
-                        height: displayHeight
-                    });
-
-                    $("#displayCanvasBox").css({
-                        width: displayWidth + ($("#displayInfo").outerWidth() + 10),
-                        height: displayHeight,
-                        marginTop: displayHeight / -2,
-                        marginLeft: (displayWidth + $("#displayInfo").outerWidth() + 10) / -2
-                    });
-                } catch (e) {
-                    console.log("ERROR! " + e.toString());
-                }
+                helloDisplay.showSketch(gistSource);
             }
         });
+    },
+    /**
+     * Fetch sketch code Parse
+     */    
+    displayGallery: function (parseID) {    
+
+        Parse.initialize("x8FmMInL8BbVeBqonPzgvS8WNKbPro65Li5DzTI0", "Y7PPNnhLPhCdFMAKgw7amBxGerz67gAnG3UKb53s");
+
+        var GalleryObject = Parse.Object.extend("Gallery");
+        var query = new Parse.Query(GalleryObject);
+        query.get(parseID, {
+          success: function(gallery) {
+            helloDisplay.showSketch(gallery.get("source"));
+          },
+          error: function(object, error) {
+            // The object was not retrieved successfully.
+            // error is a Parse.Error with an error code and message.
+          }
+        });
+
+    },
+    /**
+     * Feed sourceto Processing.js
+     */
+    showSketch: function (source) {
+
+        var processingCanvas,
+            processingInstance,
+            displayHeight,
+            displayWidth;
+
+        helloDisplay.processingSource = source;
+        helloDisplay.editor.setValue(source, -1);
+
+        try {
+            processingCanvas = document.getElementById("displayCanvas");
+            processingInstance = new Processing(processingCanvas, source);
+
+            displayHeight = 400;
+            displayWidth = (processingInstance.width / processingInstance.height) * displayHeight;
+
+            $("#displayCanvas").css({
+                width: displayWidth,
+                height: displayHeight
+            });
+
+            $("#displayCanvasBox").css({
+                width: displayWidth + ($("#displayInfo").outerWidth() + 10),
+                height: displayHeight,
+                marginTop: displayHeight / -2,
+                marginLeft: (displayWidth + $("#displayInfo").outerWidth() + 10) / -2
+            });
+        } catch (e) {
+            console.log("ERROR! " + e.toString());
+        }
+
     }
 };
